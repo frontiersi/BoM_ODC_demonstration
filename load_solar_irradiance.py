f05746d55fcb14b97bb1d2a00dda28710ead34c5
lavender-liu <whusggliuqx@gmail.com>
2023-08-27 02:24:20 +0000

    add description update function

--- load_solar_irradiance.py
+++ load_solar_irradiance.py
@@ -1,11 +1,20 @@
 import xarray as xr
 import numpy as np
 import pandas as pd
+from tqdm import tqdm
 # function to load solar irradiance data
 
-def load_solar_irradiance(lat, lon, time, var_names = ['daily_integral_of_surface_global_irradiance'], grid = 'nearest'):
-    product_baseurl = 'https://dapds00.nci.org.au/thredds/dodsC/rv74/satellite-products/arc/der/himawari-ahi/solar/p1d/latest'
-    
+def load_solar_irradiance(lat, lon, time, var_names = ['daily_integral_of_surface_global_irradiance'],
+                          grid = 'nearest',product_baseurl = 'https://dapds00.nci.org.au/thredds/dodsC/rv74/satellite-products/arc/der/himawari-ahi/solar/p1d/latest'):
+    '''
+    Retrive solar radiation products from THREDDS server
+    Parameters:
+        lat: tuple, latitude range
+        lon: tuple, longitude range
+        var_names: list of strings, mesurements/observations
+        grid: string, snap to nearest grid
+        product_baseurl: string, base url of the THREDDS directory
+    '''
     # lat, lon grid
     if grid == 'nearest':
         # select lat/lon range from data; snap to nearest grid
@@ -16,13 +25,13 @@ def load_solar_irradiance(lat, lon, time, var_names = ['daily_integral_of_surfac
         lon_range = np.arange(np.min(np.floor(np.array(lon)*10.-0.5)/10.+0.05), np.max(np.ceil(np.array(lon)*10.+0.5)/10.-0.05)+0.05, 0.1)
     daterange = pd.date_range(time[0],time[1])
     datasets = []
-    for single_date in daterange:
+    for single_date in tqdm(daterange):
         # retrive year, month and day for directory construction
         year=str(single_date.year)
         month=str(single_date.month).zfill(2)
         day=str(single_date.day).zfill(2)
         product_url='/'.join([product_baseurl,year,month,day])+'/IDE02326.'+year+month+day+'0000.nc'
-        print(product_url)
+        # print(product_url)
         
         # data is loaded lazily through OPeNDAP
         ds = xr.open_dataset(product_url)
