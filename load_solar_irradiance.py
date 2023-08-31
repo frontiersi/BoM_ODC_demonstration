31a20b4a6685ee4b41aad136d5283c6d0f894b22
lavender-liu <whusggliuqx@gmail.com>
2023-08-31 04:22:43 +0000

    update data load function to remove out of date range data

--- load_solar_irradiance.py
+++ load_solar_irradiance.py
@@ -27,15 +27,19 @@ def load_solar_irradiance(lat, lon, time, var_names = ['daily_integral_of_surfac
         # print(product_url)
         # data is loaded lazily through OPeNDAP
         with xr.open_dataset(product_url) as ds:      
-            # slice before return: expand slice limit a bit so inclusive of query range
-            ds_sliced = ds[var_names].sel(latitude=slice(lat[0],lat[1]+pixel_buffer), longitude=slice(lon[0],lon[1]+pixel_buffer)).compute()
-            datasets.append(ds_sliced)
+            if (ds.isel(time=0).time.values<pd.to_datetime(time[0])) or (ds.isel(time=0).time.values>pd.to_datetime(time[1])):
+                print('Dataset time attribute out of queried time range, skipping it...')
+            else:
+                # slice before return: expand slice limit a bit so inclusive of query range
+                ds_sliced = ds[var_names].sel(latitude=slice(lat[0],lat[1]+pixel_buffer), 
+                                              longitude=slice(lon[0],lon[1]+pixel_buffer)).compute()
+                datasets.append(ds_sliced)
     print('merging datasets...')
     return xr.concat(datasets,dim='time')
     # return xr.merge(datasets)
 
 def load_summarise_solar_irradiance(lat, lon, time, var_names = ['daily_integral_of_surface_global_irradiance'],
-                          timestep='3m',product_baseurl = 'https://dapds00.nci.org.au/thredds/dodsC/rv74/satellite-products/arc/der/himawari-ahi/solar/p1d/latest',
+                          timestep='3MS',product_baseurl = 'https://dapds00.nci.org.au/thredds/dodsC/rv74/satellite-products/arc/der/himawari-ahi/solar/p1d/latest',
                          pixel_buffer=0.02):
     '''
     Retrive solar radiation products from THREDDS server
@@ -43,7 +47,7 @@ def load_summarise_solar_irradiance(lat, lon, time, var_names = ['daily_integral
         lat: tuple, latitude range
         lon: tuple, longitude range
         var_names: list of strings, mesurements/observations
-        timestep: string, timestep for temporal aggregation, e.g. '2m' for 2 months
+        timestep: string, timestep for temporal aggregation, e.g. '2M' for 2 months frequency resampled from month start dates
         product_baseurl: string, base url of the THREDDS directory
         pixel_buffer: float, a buffer of distance in degrees applied to slicing the dataset so that it includes the boundary pixels
     '''
@@ -59,10 +63,13 @@ def load_summarise_solar_irradiance(lat, lon, time, var_names = ['daily_integral
         # print(product_url)
         # data is loaded lazily through OPeNDAP
         with xr.open_dataset(product_url,decode_coords="all") as ds:
-            # slice before return: expand slice limit a bit so inclusive of query range
-            ds_sliced = ds[var_names].sel(latitude=slice(lat[0],lat[1]+pixel_buffer), 
-                                          longitude=slice(lon[0],lon[1]+pixel_buffer))
-            datasets.append(ds_sliced)
+            if (ds.isel(time=0).time.values<pd.to_datetime(time[0])) or (ds.isel(time=0).time.values>pd.to_datetime(time[1])):
+                print('Dataset time attribute out of queried time range, skipping it...')
+            else:
+                # slice before return: expand slice limit a bit so inclusive of query range
+                ds_sliced = ds[var_names].sel(latitude=slice(lat[0],lat[1]+pixel_buffer), 
+                                              longitude=slice(lon[0],lon[1]+pixel_buffer))
+                datasets.append(ds_sliced)
     print('merging dataset...')
     # datasets=xr.merge(datasets)
     datasets=xr.concat(datasets,dim='time')
